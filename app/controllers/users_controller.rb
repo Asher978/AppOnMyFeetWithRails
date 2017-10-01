feaca18c0334ba9e6bac6394272f765dfdda03bc
Asher Shaheen <ashershaheen@gmail.com>
2017-09-30 22:33:41 -0400

    updated JSON response in profile method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,14 @@ class UsersController < ApiController
       def profile
         user = User.find_by_auth_token!(request.headers[:token])
         user_runs = Run.where(user_id: user.id)
-        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname },
+        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname, half_marathons: user.half_marathons,
+        full_marathons: user.full_marathons, total_miles: user.total_miles, picture: user.picture, admin: user.admin },
         runs: user_runs,
       }
       end
     
       private
       def user_params
-        params.require(:user).permit(:username, :firstname, :lastname, :email, :password)
+        params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :half_marathons, :full_marathons, :total_miles, :picture, :admin)
       end
 end

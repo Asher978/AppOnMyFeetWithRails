2fd558e1e9774108a96c45e8c0ead03e4a0bfa91
Asher Shaheen <ashershaheen@gmail.com>
2017-09-30 20:58:41 -0400

    added method to create users and view their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApiController
+    before_action :require_login, except: [:create]
+    
+      def create
+        user = User.create!(user_params)
+        render json: { token: user.auth_token }
+      end
+    
+      def profile
+        user = User.find_by_auth_token!(request.headers[:token])
+        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname } }
+      end
+    
+      private
+      def user_params
+        params.require(:user).permit(:username, :firstname, :lastname, :email, :password)
+      end
 end

5b621248d65530d98dd5866519e141cb50d799d4
Asher Shaheen <ashershaheen@gmail.com>
2017-09-30 20:28:08 -0400

    ran migration for users table

--- db/migrate/20171001001916_create_users.rb
+++ db/migrate/20171001001916_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :username, index: true
+      t.string :firstname
+      t.string :lastname
+      t.string :password_digest
+      t.string :email
+      t.string :auth_token
 
       t.timestamps
     end

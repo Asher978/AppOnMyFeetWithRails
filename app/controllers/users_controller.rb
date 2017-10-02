db28c7db27ab75f166523534b9848e1f06552f23
Asher Shaheen <ashershaheen@gmail.com>
2017-10-01 22:03:30 -0400

    updated JSON response of users based on removing columns from the user table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,13 @@ class UsersController < ApiController
       def profile
         user = User.find_by_auth_token!(request.headers[:token])
         user_runs = Run.where(user_id: user.id)
-        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname, half_marathons: user.half_marathons,
-        full_marathons: user.full_marathons, total_miles: user.total_miles, picture: user.picture, admin: user.admin },
+        render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname, admin: user.admin },
         runs: user_runs,
       }
       end
     
       private
       def user_params
-        params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :half_marathons, :full_marathons, :total_miles, :picture, :admin)
+        params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :admin)
       end
 end

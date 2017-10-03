f6284a373afc0d554a7c42b8a73223d913d86279
Asher Shaheen <ashershaheen@gmail.com>
2017-10-03 18:37:44 -0400

    added user profile in the json response to front end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApiController
       def profile
         user = User.find_by_auth_token!(request.headers[:token])
         user_runs = Run.where(user_id: user.id)
+        user_profile = user.profile
         render json: { user: { username: user.username, email: user.email, firstname: user.firstname, lastname: user.lastname, admin: user.admin },
         runs: user_runs,
+        profile: user_profile,
       }
       end
     

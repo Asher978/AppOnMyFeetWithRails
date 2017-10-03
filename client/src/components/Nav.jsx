a6e8606067310f9172fc672ed08842a044800414
Asher Shaheen <ashershaheen@gmail.com>
2017-10-03 10:40:48 -0400

    changed the navbar to bootstrap

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { Menu, Segment } from 'semantic-ui-react'
 import Auth from '../modules/Auth';
 
 const Nav = props => {
     return (
-        <Segment inverted>
-        <Menu inverted secondary>
-            <Menu.Item><Link to="/">Home</Link></Menu.Item>
-            {!Auth.isUserAuthenticated() ? (
-                <Menu inverted secondary>
-                    <Menu.Item><Link to="/login">Login</Link></Menu.Item>
-                    <Menu.Item><Link to="/register">Register</Link></Menu.Item>
-                </Menu>
-            ) : (
-                <Menu.Item><Link to="/logout" onClick={props.logoutUser}>Log Out</Link></Menu.Item>
-            )}
-        </Menu>
-        </Segment>
+        <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+                <div className="navbra-header">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" href="#"><img src="" alt="" /></a>
+                </div>
+                <div className="collapse navbar-collapse" id="MyNavbar">
+                    {!Auth.isUserAuthenticated() ? (
+                        <ul className="nav navbar-nav navbar right">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                        </ul>
+                        ) : (
+                            <ul className="nav navbar-nav navbar right">
+                                <li><Link to="/">Home</Link></li>                                
+                                <li><Link to="/profile">My Profile</Link></li>
+                                <li><Link to="/logout" onClick={props.logoutUser}>Log Out</Link></li>
+                            </ul>
+                        )}
+                </div>
+            </div>
+        </nav>
     )
 }
 

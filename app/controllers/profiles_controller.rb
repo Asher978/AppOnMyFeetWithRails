fd3c77d693b53fbd5e6e052e5deb5d5d1e089a66
Asher Shaheen <ashershaheen@gmail.com>
2017-10-03 18:43:25 -0400

    added profile table and migrated and created controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApiController
+
+    def show
+        current_user
+    end
+
+    def update
+
+    end
+end
---
+++ app/models/profile.rb
@@ -0,0 +1,3 @@
+class Profile < ApplicationRecord
+    belongs_to :user, foreign_key: 'user_id'
+end

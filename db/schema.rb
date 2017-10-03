b552861f1dc616b08bf2b310f23e70236713c8ce
Asher Shaheen <ashershaheen@gmail.com>
2017-10-03 18:38:29 -0400

    updated schema due to migration of profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002015610) do
+ActiveRecord::Schema.define(version: 20171002173804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "full_marathons"
+    t.integer "half_marathons"
+    t.integer "total_miles"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "runs", force: :cascade do |t|
     t.date "run_date"
@@ -27,6 +36,8 @@ ActiveRecord::Schema.define(version: 20171002015610) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_runs_on_user_id"
   end
 

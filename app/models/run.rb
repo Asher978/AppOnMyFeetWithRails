7354ad00c8f7970070588dca8f1c899bb6a45078
Asher Shaheen <ashershaheen@gmail.com>
2017-10-03 18:44:04 -0400

    added the model for runs

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,9 @@
 class Run < ApplicationRecord
     belongs_to :user
+    geocoded_by :first_intersection
+    after_validation :geocode
+
+    def first_intersection
+        [starting_street1, staring_city].compact.join(', ')
+    end
 end

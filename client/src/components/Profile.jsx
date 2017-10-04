9eb55e99248bd1656b422ef464812f7906c79104
Asher Shaheen <ashershaheen@gmail.com>
2017-10-04 16:33:42 -0400

    added users run overview

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Auth from '../modules/Auth';
 import logo from '../Mypic.jpeg';
+import { Link } from 'react-router-dom';
+
 
 class Profile extends Component {
     constructor () {
         super();
         this.state = {
             profileData: null,
+            runsData: null,
             profileDataLoaded: false,
         }
     }
 
     componentDidMount() {
+        this.props.resetFireRedirect()
         axios('/profile', {
             method: 'GET',
             headers: {
@@ -23,9 +27,27 @@ class Profile extends Component {
             console.log(res.data)
             this.setState({
               profileData: res.data.user,
+              runsData: res.data.runs,
               profileDataLoaded: true,
             })
         })
+    }
+
+    renderRuns () {
+        if (this.state.profileDataLoaded) {
+            return (
+                this.state.runsData.map(run => {
+                    return (
+                        <tr key={run.id}>
+                            <td>{run.run_date}</td>
+                            <td>{run.miles}</td>
+                            <td>{run.starting_point}, {run.starting_city}</td>
+                            <td>{run.ending_point}, {run.ending_city}</td>
+                        </tr>
+                    )
+                })
+            )
+        }
     }
 
     renderProfile () {
@@ -36,8 +58,7 @@ class Profile extends Component {
                         <div className="container">
                             <div className="row">
                                 <div className="col-md-10">
-                                    <h1>Hello {this.state.profileData.firstname}<img className="pic" src={logo} /></h1>
-                                    
+                                    <h1>Hello {this.state.profileData.firstname}<img className="pic" src={logo} alt={this.state.profileData.firstname}/></h1>
                                     <h4>Welcome to your running Profile</h4>
                                 </div>
                                 <div className="col-md-2">
@@ -47,8 +68,8 @@ class Profile extends Component {
                                             <span className="caret"></span>
                                         </button>
                                         <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-                                            <li><a href="#">Update Profile</a></li>
-                                            <li><a href="#">Add Profile Picture</a></li>
+                                            <li><a href="">Update Profile</a></li>
+                                            <li><a href="">Add Profile Picture</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -58,7 +79,7 @@ class Profile extends Component {
                     <section id="breadcrumb">
                         <div className="container">
                             <ol className="breadcrumb">
-                                <li>My Running History</li>
+                                <li>Your Running History</li>
                             </ol>
                         </div>
                     </section>
@@ -67,12 +88,12 @@ class Profile extends Component {
                             <div className="row">
                                 <div className="col-md-3">
                                 <div className="list-group">
-                                    <a href="#" className="list-group-item active main-color-bg">
+                                    <a href="" className="list-group-item active main-color-bg">
                                     <span className="glyphicon glyphicon-hand-right" aria-hidden="true"></span> Last Run
                                     </a>
-                                    <a href="#" className="list-group-item"><span className="glyphicon glyphicon-random" aria-hidden="true"></span>&nbsp;All Runs <span className="badge">21</span></a>
-                                    <a href="#" className="list-group-item"><span className="glyphicon glyphicon-adjust" aria-hidden="true"></span>&nbsp;Half Marathons <span className="badge">2</span></a>
-                                    <a href="#" className="list-group-item"><span className="glyphicon glyphicon-cd" aria-hidden="true"></span>&nbsp;Full Marathons <span className="badge">1</span></a>
+                                    <Link to="/runslist" className="list-group-item"><span className="glyphicon glyphicon-random" aria-hidden="true"></span>&nbsp;All Runs <span className="badge">{this.state.runsData.length}</span></Link>
+                                    <a href="" className="list-group-item"><span className="glyphicon glyphicon-adjust" aria-hidden="true"></span>&nbsp;Half Marathons <span className="badge">2</span></a>
+                                    <a href="" className="list-group-item"><span className="glyphicon glyphicon-cd" aria-hidden="true"></span>&nbsp;Full Marathons <span className="badge">1</span></a>
                                     </div>
                                 </div>
                                 <div className="col-md-9">
@@ -83,7 +104,7 @@ class Profile extends Component {
                                     <div className="panel-body">
                                         <div className="col-md-4">
                                             <div className="well dash-box">
-                                                <h2><span className="glyphicon glyphicon-random" aria-hidden="true"></span> 21</h2>
+                                                <h2><span className="glyphicon glyphicon-random" aria-hidden="true"></span> {this.state.runsData.length}</h2>
                                                 <h4>All Runs</h4>
                                             </div>
                                         </div>
@@ -101,6 +122,27 @@ class Profile extends Component {
                                         </div>
                                     </div>
                                     </div>
+
+                                    {/* RUNS OVERVIEW */}
+                                    <div className="panel panel-default">
+                                        <div className="panel-heading main-color-bg">
+                                            <h3 className="panel-title">Runs Snapshot</h3>
+                                        </div>
+                                        <div className="panel-body">
+                                            <table className="table table-striped table-hover">
+                                                <tbody>
+                                                <tr>
+                                                    <th>Run Date</th>
+                                                    <th>Miles Covered</th>
+                                                    <th>Starting Location</th>
+                                                    <th>Ending Location</th>
+                                                </tr>
+                                                {this.renderRuns()}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -109,14 +151,17 @@ class Profile extends Component {
             )
         }
     }
+    
 
     render () {
         return (
             <div>
                 {this.renderProfile()}
+                
             </div>
         )
     }
 }
+
 
 export default Profile;

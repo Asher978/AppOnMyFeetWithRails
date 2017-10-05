076272a6ef4f7392924da8fb3a092f11187b3f4f
Asher Shaheen <ashershaheen@gmail.com>
2017-10-04 21:22:58 -0400

    added a column in profile table for picture and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004004729) do
+ActiveRecord::Schema.define(version: 20171005002838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171004004729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
   end
 
   create_table "runs", force: :cascade do |t|

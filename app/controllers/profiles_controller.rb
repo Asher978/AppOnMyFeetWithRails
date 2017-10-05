f9cf7ef87fed247ff288a51e50452e3e58b90b54
Asher Shaheen <ashershaheen@gmail.com>
2017-10-04 21:26:25 -0400

    added methods for show and update profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,19 @@
 class ProfilesController < ApiController
 
     def show
-        current_user
+        user = User.find_by_auth_token!(request.headers[:token])
+        user_profile = user.profile
+        render json: { profile: user_profile }
     end
 
     def update
+        user = current_user
+        user.profile.update!(update_params)
+        render json: { profile: user.profile }
+    end
 
+    private
+    def update_params
+        params.require(:profile).permit(:half_marathons, :full_marathons, :picture)
     end
 end

8b1c4d4810546ae5175c00a648d57b087e64f0fc
Asher Shaheen <ashershaheen@gmail.com>
2017-10-12 10:14:37 -0400

    created the start run component

---
+++ client/src/components/RunStart.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class RunStart extends Component {
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default RunStart;

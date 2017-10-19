e131ed02c9b2ac02e13f1288cd656d101248414a
asher978 <ashershaheen@gmail.com>
2017-10-19 00:13:48 -0400

    added controller for trsckruns with methods  to create trackeuns

---
+++ app/controllers/trackruns_controller.rb
@@ -0,0 +1,33 @@
+class TrackrunsController < ApiController
+  before_action :require_login, except: [:index, :show]
+
+  def index
+    trackruns = current_user.trackruns.all
+    render json: {trackruns: trackruns}
+  end
+
+  def create
+    trackrun = Trackrun.new
+    trackrun.rundata = trackruns_params
+    trackrun.user = current_user
+    
+    if trackrun.save
+      render json: {
+        message: 'ok',
+        trackrun: trackrun,
+      }
+    else
+      render json: {message: 'Could not create run'}
+    end
+
+  end
+
+
+  private
+    def trackruns_params
+      a = params.require(:trackrun).permit!
+      a
+    end
+
+
+end

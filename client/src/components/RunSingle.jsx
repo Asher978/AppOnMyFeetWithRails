00f610cda3fe5b438eb879776b2d3f98cc15346b
asher978 <ashershaheen@gmail.com>
2017-10-22 23:37:49 -0400

    logic to render the single run

--- client/src/components/RunSingle.jsx
+++ client/src/components/RunSingle.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const RunSingle = props => {
-  return (
-      <div className="thumbnail">
-          {props.renderMap(props.rundata)}
-      <div className="caption">
-          <h3 className="main-color-bg rundate">{`${moment(props.created).format('MMM D, Y')}`}</h3>
-          <p>{props.miles} miles</p>
-          <p>Your run was from {props.start} to {props.end}.</p>
-          <a href="" className="btn main-color-bg" role="button">View this Run!</a>
+class RunSingle extends Component {
+  constructor () {
+    super ();
+  }
+
+  ComponentDidMount () {
+    
+  }
+
+  render () {
+    return (
+      <div>
+
       </div>
-      </div>
-  )
+    )
+  }
 }
 
 export default RunSingle;
